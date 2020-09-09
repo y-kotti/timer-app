2230677a4c17dc10fdf045fa702f3f72b411487c
y-kotti <garoyann1@gmail.com>
2020-09-10 06:32:47 +0900

    カウントダウンタイマーを作成

--- src/views/CountDown.vue
+++ src/views/CountDown.vue
@@ -3,8 +3,17 @@
     <div class="time">
       {{ formatTime }}
     </div>
-    <button v-on:click="start" v-if="!timerOn">Start</button>
-    <button v-on:click="stop" v-if="timerOn">Stop</button>
+    <div>
+      <button v-on:click="startTimer" v-if="!timerOn">Start</button>
+      <button v-on:click="stopTimer" v-if="timerOn">Stop</button>
+      <button v-on:click="resetTimer">Reset</button>
+    </div>
+    <div>
+      <button v-on:click="increaseTimeByOneMinute">1分+</button>
+      <button v-on:click="decreaseTimeByOneMinute">1分-</button>
+      <button v-on:click="increaseTimeByOneSecond">1秒+</button>
+      <button v-on:click="decreaseTimeByOneSecond">1秒-</button>
+    </div>
   </div>
 </template>
 
@@ -13,46 +22,89 @@ export default {
   name: "count-down",
   data() {
     return {
-      min: 30,
-      sec: 0,
+      setMin: 0,
+      setSec: 0,
+      currentMin: 0,
+      currentSec: 0,
       timerOn: false,
       timerObj: null,
     };
   },
   methods: {
-    count: function() {
-      if (this.sec <= 0 && this.min >= 1) {
-        this.min--;
-        this.sec = 59;
-      } else if (this.sec <= 0 && this.min <= 0) {
-        this.complete();
+    timeCount: function() {
+      if (this.currentSec <= 0 && this.currentMin >= 1) {
+        this.currentMin--;
+        this.currentSec = 59;
+      } else if (this.currentSec <= 0 && this.currentMin <= 0) {
+        this.timeUp();
       } else {
-        this.sec--;
+        this.currentSec--;
       }
     },
 
-    start: function() {
+    startTimer: function() {
+      this.setMin = this.currentMin;
+      this.setSec = this.currentSec;
+      if (this.setMin == 0 && this.setSec == 0) {
+        return;
+      }
+
       let self = this;
       this.timerObj = setInterval(function() {
-        self.count();
+        self.timeCount();
       }, 1000);
-      this.timerOn = true; //timerがOFFであることを状態として保持
+      this.timerOn = true;
     },
 
-    stop: function() {
+    stopTimer: function() {
       clearInterval(this.timerObj);
-      this.timerOn = false; //timerがOFFであることを状態として保持
+      this.timerOn = false;
     },
 
-    complete: function() {
+    timeUp: function() {
+      if (this.setMin != 0 || this.setSec != 0) {
+        alert("Time Up");
+      }
+      this.timerOn = false;
       clearInterval(this.timerObj);
+    },
+
+    resetTimer: function() {
+      clearInterval(this.timerObj);
+      this.currentMin = this.setMin;
+      this.currentSec = this.setSec;
+    },
+
+    increaseTimeByOneMinute: function() {
+      if (this.currentMin < 59) {
+        this.currentMin += 1;
+      }
+    },
+
+    decreaseTimeByOneMinute: function() {
+      if (0 < this.currentMin) {
+        this.currentMin -= 1;
+      }
+    },
+
+    increaseTimeByOneSecond: function() {
+      if (this.currentSec < 59) {
+        this.currentSec += 1;
+      }
+    },
+
+    decreaseTimeByOneSecond: function() {
+      if (0 < this.currentSec) {
+        this.currentSec -= 1;
+      }
     },
   },
   computed: {
     formatTime: function() {
-      let timeStrings = [this.min.toString(), this.sec.toString()].map(function(
-        str
-      ) {
+      let timeStrings = [
+        this.currentMin.toString(),
+        this.currentSec.toString(),
+      ].map(function(str) {
         if (str.length < 2) {
           return "0" + str;
         } else {
